376ec6104119d797b85d3ec38bc954799619c1d7
Nadine <152076100+nadine-grantz@users.noreply.github.com>
2023-12-10 20:38:01 +0000

    add toggle Button Answer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,37 @@
-const zeigAntwortButton = document.querySelector('[data-js="zeig-antwort"]');
-
-zeigAntwortButton.addEventListener("click", () => {
+const antwortElement1 = document.querySelector('[data-js="zeig-antwort1"]');
+const buttonElement1 = document.querySelector('[data-js="Button1"]');
     
-});
+    buttonElement1.addEventListener('click', () => {
+        if (antwortElement1.style.display === 'none') {
+            antwortElement1.style.display = 'block';
+        } else {
+            antwortElement1.style.display = 'none';
+        }
+    });
+
+const antwortElement2 = document.querySelector('[data-js="zeig-antwort2"]');
+const buttonElement2 = document.querySelector('[data-js="Button2"]');
+        
+        buttonElement2.addEventListener('click', () => {
+            if (antwortElement2.style.display === 'none') {
+                antwortElement2.style.display = 'block';
+            } else {
+                antwortElement2.style.display = 'none';
+            }
+        });
+
+const antwortElement3 = document.querySelector('[data-js="zeig-antwort3"]');
+const buttonElement3 = document.querySelector('[data-js="Button3"]');
+            
+            buttonElement3.addEventListener('click', () => {
+                if (antwortElement3.style.display === 'none') {
+                    antwortElement3.style.display = 'block';
+                } else {
+                    antwortElement3.style.display = 'none';
+                }
+            });
+
+
+
+   
+    
